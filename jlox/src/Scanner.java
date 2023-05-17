03e8c414f406df4e8f802d497c1cf5237a60b574
Shivam Kajaria <shivam.kajaria@rutgers.edu>
2023-05-16 23:00:27 -0400

    Chapter 4 Code + Challenge

---
+++ jlox/src/Scanner.java
@@ -0,0 +1,250 @@
+package out;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static out.TokenType.*;
+
+class Scanner {
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
+    private int start = 0; // points to character at start of current lexeme
+    private int current = 0; // points to character being considered
+    private int line = 1; // is the line we are on in the source
+
+    private static final Map<String, TokenType> keywords;
+
+    static {
+        keywords = new HashMap<String, TokenType>();
+        keywords.put("and", AND);
+        keywords.put("class", CLASS);
+        keywords.put("else", ELSE);
+        keywords.put("false", FALSE);
+        keywords.put("for", FOR);
+        keywords.put("fun", FUN);
+        keywords.put("if", IF);
+        keywords.put("nil", NIL);
+        keywords.put("or", OR);
+        keywords.put("print", PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super", SUPER);
+        keywords.put("this", THIS);
+        keywords.put("true", TRUE);
+        keywords.put("var", VAR);
+        keywords.put("while", WHILE);
+    }
+
+    Scanner(String source) {
+        this.source = source;
+    }
+
+    List<Token> scanTokens() {
+        while (!isAtEnd()) {
+            start = current;
+            scanToken();
+        }
+
+        tokens.add(new Token(EOF, "", null, line));
+        return tokens;
+    }
+
+    private boolean isAtEnd() {
+        return current >= source.length();
+    }
+
+    private void scanToken() {
+        char c = advance();
+
+        // checking before whitespace since they are note white space determined
+        // maximal munch in use here. If two strings match, the longer one wins.
+        switch (c) {
+            case '(':
+                addToken(LEFT_PAREN);
+                break;
+            case ')':
+                addToken(RIGHT_PAREN);
+                break;
+            case '{':
+                addToken(LEFT_BRACE);
+                break;
+            case '}':
+                addToken(RIGHT_BRACE);
+                break;
+            case ',':
+                addToken(COMMA);
+                break;
+            case '.':
+                addToken(DOT);
+                break;
+            case '-':
+                addToken(MINUS);
+                break;
+            case '+':
+                addToken(PLUS);
+                break;
+            case ';':
+                addToken(SEMICOLON);
+                break;
+            case '*':
+                addToken(STAR);
+            case '!':
+                addToken(match('=') ? BANG_EQUAL : BANG);
+                break;
+            case '<':
+                addToken(match('=') ? LESS_EQUAL : LESS);
+                break;
+            case '>':
+                addToken(match('=') ? GREATER_EQUAL : GREATER);
+                break;
+            case '=':
+                addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+                break;
+            case '/':
+                if (match('/')) {
+                    while (peek() != '\n' && !isAtEnd())
+                        advance();
+                } else if (match('*')) {
+                    int open = 1;
+                    while (open > 0 && !isAtEnd()) {
+                        c = advance();
+
+                        switch (c) {
+                            case '/':
+                                if (match('*'))
+                                    open++;
+                                break;
+                            case '*':
+                                if (match('/'))
+                                    open--;
+                                break;
+                            case '\n':
+                                line++;
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                } else {
+                    addToken(SLASH);
+                }
+                break;
+            case '\n':
+                line++;
+            case ' ':
+            case '\r':
+            case '\t':
+                break;
+            case '"':
+                string();
+                break;
+            default:
+                if (isDigit(c)) {
+                    number();
+                } else if (isAlpha(c)) {
+                    identifier();
+                } else {
+                    // This will give an error for each unexpected character. For contiguous
+                    // unexpected characters we should only give error for better UX.
+                    Lox.error(line, "Unexpected character.");
+                    break;
+                }
+        }
+    }
+
+    private void identifier() {
+        while (isAlphaNumeric(peek()))
+            advance();
+
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if (type == null)
+            type = IDENTIFIER;
+
+        addToken(type);
+    }
+
+    // TODO allow leading and trailing decimal (will be very annoying)
+    private void number() {
+        while (isDigit(peek()))
+            advance();
+
+        if (peek() == '.' && isDigit(peekNext())) {
+            advance();
+
+            while (isDigit(peek()))
+                advance();
+        }
+
+        addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+    }
+
+    private char peekNext() {
+        if (current + 1 >= source.length())
+            return '\0';
+        return source.charAt(current + 1);
+    }
+
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') ||
+                (c >= 'A' && c <= 'Z') ||
+                c == '_';
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
+    }
+
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
+    }
+
+    // TODO Could be cool to add in escape sequences
+    private void string() {
+        while (peek() != '"' && !isAtEnd()) {
+            if (peek() == '\n')
+                line++;
+            advance();
+        }
+
+        if (isAtEnd()) {
+            Lox.error(line, "Unterminated string.");
+            return;
+        }
+
+        advance();
+
+        String value = source.substring(start + 1, current - 1);
+        addToken(STRING, value);
+    }
+
+    private char peek() {
+        if (isAtEnd())
+            return '\0';
+        return source.charAt(current);
+    }
+
+    private boolean match(char expected) {
+        if (isAtEnd()) // advance iterates current, so this is checking for the next char
+            return false;
+        if (source.charAt(current) != expected)
+            return false;
+
+        current++;
+        return true;
+    }
+
+    private char advance() {
+        return source.charAt(current++);
+    }
+
+    private void addToken(TokenType type) {
+        addToken(type, null);
+    }
+
+    private void addToken(TokenType type, Object literal) {
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
+    }
+}
--- jlox/src/main.java
+++
@@ -1,7 +0,0 @@
-package out;
-
-class main {
-	public static void main (String[] args) {
-		System.out.println("Hellow, World!");
-	}
-}
